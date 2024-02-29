4055d8ccdaa2c125312a1866e7c1efda937ab064
unknown <persnb@gmail.com>
2024-03-01 00:12:04 +0330

    set swagger config

--- src/modules/user/user.model.js
+++ src/modules/user/user.model.js
@@ -1,18 +1,18 @@
 const { Schema, Model, Schema } = require("mongoose");
 
 const OTPSchema = new Schema({
-    code: {type: String, required: false, default: undefined},
-    expiresIn: {type: Number, required: false, default: 0}
+    code: { type: String, required: false, default: undefined },
+    expiresIn: { type: Number, required: false, default: 0 }
 })
 const UserSchema = new Schema({
     name: { type: String, required: true },
-    userName: { type: String, required: true },
-    phone: { type: String, required: true },
+    userName: { type: String, required: true, lowercase: true },
+    phone: { type: String, required: true ,minLength:10,maxLength:12 },
     otp: { type: OTPSchema },
-    verifiedMobile: {type: Boolean, default: false, required: true},
-    accessToken: {type: String},
+    verifiedMobile: { type: Boolean, default: false, required: true },
+    accessToken: { type: String },
 }, {
     timestamps: true
 })
-const UserModel = model("user",UserSchema);
+const UserModel = model("user", UserSchema);
 module.exports = UserModel;
