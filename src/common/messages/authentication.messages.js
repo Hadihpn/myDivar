d1b95359aeda6cebfca845356a27883c0a45e90e
unknown <persnb@gmail.com>
2024-03-03 23:03:28 +0330

    add authorization

---
+++ src/common/messages/authentication.messages.js
@@ -0,0 +1,7 @@
+const AuthorizationMessage  = Object.freeze({
+    login: "you have logged in successfully",
+    logout: "you have logged out successfully",
+    UnAuthorize:"UnAuthorize . plz login on your account",
+    NotFoundAccount:"NotFoundAccount"
+})
+module.exports = AuthorizationMessage
